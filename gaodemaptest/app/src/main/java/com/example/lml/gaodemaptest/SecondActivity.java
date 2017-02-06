61c578241ef50ef418cde8cd2990ba8a755c4895
alianceFail <465848561@qq.com>
2017-02-07 00:51:17 +0800

    first commit

---
+++ gaodemaptest/app/src/main/java/com/example/lml/gaodemaptest/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.lml.gaodemaptest;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LML on 2017/1/5.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
